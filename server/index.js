84a346b5e4ab214986edd5f038c9e21a469d1eaf
Jayesh <jayeshpadwal09@gmail.com>
2024-11-04 12:47:36 +0530

    Route dashboard added, middleware created

--- server/index.js
+++ server/index.js
@@ -4,10 +4,16 @@ import express from "express";
 const app = express();
 dotenv.config();
 
+app.use(express.urlencoded({extended: false}));
+
 const PORT = process.env.PORT;
 app.get('/', (req , res)=>{
     res.send("Welcome to expense tracker");
 });
+
+app.get('/dashboard', (req, res)=>{
+    res.send('Expense tracker dashboard');
+})
 
 app.listen(PORT, (error)=>{
     console.log(`Server running on http://localhost:${PORT}`);
