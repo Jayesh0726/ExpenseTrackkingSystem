b2ee12335b8981e0d388d0aa0182cbd24a66107b
Pratham02 <prathamshinde2137@gmail.com>
2024-11-05 21:51:44 +0530

    feat: setup Expense model and basic routes for GET and POST

--- server/index.js
+++ server/index.js
@@ -1,20 +1,56 @@
 import dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./config/db.js";
+import Expense from "./models/expenseModel.js";
 
 const app = express();
 dotenv.config();
 
-app.use(express.urlencoded({extended: false}));
+// Connect to database
+connectDB();
 
-const PORT = process.env.PORT;
-app.get('/', (req , res)=>{
-    res.send("Welcome to expense tracker");
+// Middleware to parse JSON and URL-encoded data
+app.use(express.json());
+console.log("express.json middleware loaded");
+
+app.use(express.urlencoded({ extended: false }));
+console.log("express.urlencoded middleware loaded");
+
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/api/expenses", async (req, res) => {
+  try {
+    const expenses = await Expense.find();
+    res.status(200).json({ success: true, data: expenses });
+  } catch (error) {
+    console.log("Error fetching expenses: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
-app.get('/dashboard', (req, res)=>{
-    res.send('Expense tracker dashboard');
-})
+app.post("/api/expenses", async (req, res) => {
+  const newExpense = new Expense(req.body);
+  console.log("Request Body:", req.body);
+  try {
+    const savedExpense = await newExpense.save();
+    res.status(201).json({ success: true, data: savedExpense });
+  } catch (error) {
+    console.log("Error creating expenses: ", error.message);
+    res.status(400).json({ success: false, message: "Error adding expense" });
+  }
+});
 
-app.listen(PORT, (error)=>{
-    console.log(`Server running on http://localhost:${PORT}`);
-})
+app.post("/test", (req, res) => {
+  console.log("Received Body:", req.body);
+  res.json({ receivedBody: req.body });
+});
+
+
+app.get("/dashboard", (req, res) => {
+  res.send("Expense tracker dashboard");
+});
+
+app.listen(PORT, (error) => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
